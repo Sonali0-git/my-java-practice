56b0b9f68e6a5fa83663cc8840fe85145d84678c
Sonali Barik <sonalibarik190@gmail.com>
2026-05-22 09:26:27 +0530

    Add files via upload

---
+++ EvenElements.java
@@ -0,0 +1,19 @@
+// 4. Java program to print even elements present in an array
+// Input: {1,2,3,4,5,6}
+// Output: {2,4,6}
+
+public class EvenElements {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5, 6};
+
+        System.out.println("Even elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
