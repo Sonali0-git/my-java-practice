72fd77a9cdee320b54cad2bd1d52d3ec82e9f2fb
Sonali Barik <sonalibarik190@gmail.com>
2026-05-22 14:34:30 +0530

    Add files via upload

---
+++ OddSum.java
@@ -0,0 +1,15 @@
+// 9. Java program to print odd elements sum
+public class OddSum {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int sum = 0;
+
+        for(int i : arr) {
+            if(i % 2 != 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println("Output: " + sum);
+    }
+}
