2041ec2ce835c35212dfc7c57bb2271616363a75
Sonali Barik <sonalibarik190@gmail.com>
2026-05-25 11:04:35 +0530

    Add files via upload

---
+++ CountOccurrence.java
@@ -0,0 +1,30 @@
+// 16. Count Occurrence of Each Element in an Array
+
+import java.util.*;
+
+public class CountOccurrence {
+
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,1,3,2,4};
+
+        for(int i=0; i<arr.length; i++) {
+
+            int count = 1;
+
+            if(arr[i] == -1)
+                continue;
+
+            for(int j=i+1; j<arr.length; j++) {
+
+                if(arr[i] == arr[j]) {
+
+                    count++;
+                    arr[j] = -1;
+                }
+            }
+
+            System.out.println(arr[i] + " -> " + count);
+        }
+    }
+}
