4cdab236cf5ec6e99fecda8fbfd479d55a4aff73
Sonali Barik <sonalibarik190@gmail.com>
2026-05-25 11:19:10 +0530

    Add files via upload

---
+++ SmallestElement.java
@@ -0,0 +1,22 @@
+// 21. Find Smallest Element in an Array
+
+import java.util.*;
+
+public class SmallestElement {
+
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,1,3,2,4};
+
+        int min = arr[0];
+
+        for(int i=1; i<arr.length; i++) {
+
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Output{" + min + "}");
+    }
+}
