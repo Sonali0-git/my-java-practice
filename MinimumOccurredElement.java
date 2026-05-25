212dfd53154fe37c52c61962c170896403b4280f
Sonali Barik <sonalibarik190@gmail.com>
2026-05-25 11:10:09 +0530

    Add files via upload

---
+++ MinimumOccurredElement.java
@@ -0,0 +1,34 @@
+// 18. Find Minimum Occurred Element in an Array
+
+import java.util.*;
+
+public class MinimumOccurredElement {
+
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,1,1,2,1};
+
+        int minCount = arr.length;
+        int minElement = arr[0];
+
+        for(int i=0; i<arr.length; i++) {
+
+            int count = 0;
+
+            for(int j=0; j<arr.length; j++) {
+
+                if(arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+            if(count < minCount) {
+
+                minCount = count;
+                minElement = arr[i];
+            }
+        }
+
+        System.out.println("Output{" + minElement + "-" + minCount + "}");
+    }
+}
