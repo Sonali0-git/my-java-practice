bde3fb6676ea851e914dfd422278a0e904612412
Sonali Barik <sonalibarik190@gmail.com>
2026-05-25 11:16:05 +0530

    Add files via upload

---
+++ LargestElement.java
@@ -0,0 +1,22 @@
+// 20. Find Largest Element in an Array
+
+import java.util.*;
+
+public class LargestElement {
+
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,1,3,2,4};
+
+        int max = arr[0];
+
+        for(int i=1; i<arr.length; i++) {
+
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Output{" + max + "}");
+    }
+}
