f5974efd6e7126880e599a57706cd3c1b7f19fe1
Sonali Barik <sonalibarik190@gmail.com>
2026-05-25 16:08:17 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,14 @@
+//3. Largest of Three Numbers
+class LargestNumber {
+    public static void main(String[] args) {
+
+        int a = 10, b = 25, c = 15;
+
+        if(a > b && a > c)
+            System.out.println(a + " is largest");
+        else if(b > c)
+            System.out.println(b + " is largest");
+        else
+            System.out.println(c + " is largest");
+    }
+}
