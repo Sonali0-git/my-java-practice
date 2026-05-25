ae0a8c723fb38eab4d78ffd8ae8f78d5f1bab15f
Sonali Barik <sonalibarik190@gmail.com>
2026-05-25 15:20:37 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,26 @@
+// 51. InsertionSort
+public class  InsertionSort {
+    public static void main(String[] args) {
+
+        int a[] = {5,4,3,2,1};
+
+        for(int i=1; i<a.length; i++) {
+
+            int key = a[i];
+            int j = i - 1;
+
+            while(j >= 0 && a[j] > key) {
+                a[j+1] = a[j];
+                j--;
+            }
+
+            a[j+1] = key;
+        }
+
+        System.out.print("Sorted Array: ");
+
+        for(int i=0; i<a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
