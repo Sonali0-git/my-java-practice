74e48ffaa3ab4d6da58096ace82573e99487cc1d
Sonali Barik <sonalibarik190@gmail.com>
2026-05-26 15:13:37 +0530

    Add files via upload

---
+++ StringStudentInfo.java
@@ -0,0 +1,15 @@
+public class StringStudentInfo {
+    public static void main(String[] args) {
+
+        String name = "Sonali";
+        String regNo = "250320100077";
+        String branch = "MCA";
+        String campus = "BBSR";
+
+        System.out.println("Student Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Registration Number: " + regNo);
+        System.out.println("Branch: " + branch);
+        System.out.println("Campus: " + campus);
+    }
+}
